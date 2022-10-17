7844a8b539b42fd2392c314caadb9c5008193daa
JeffLiu114514 <jeff_jfl@163.com>
2022-10-17 16:15:18 -0400

    first

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void Recursive_Descent_parser(){
+
+}
+
+void Table_Driven_parser(){}
+
+void main(){
+    Recursive_Descent_parser();
+    Table_Driven_parser();
+}
